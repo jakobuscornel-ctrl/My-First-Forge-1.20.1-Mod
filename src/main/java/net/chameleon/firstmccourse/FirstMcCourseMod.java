03e5fb8e2cd5c014ecb3d81efc7b221ee36ccdd5
jakobuscornel-ctrl <jakobuscornel@gmail.com>
2026-09-01 15:26:42 +0200

    Just made some changes!

--- src/main/java/net/chameleon/firstmccourse/FirstMcCourseMod.java
+++ src/main/java/net/chameleon/firstmccourse/FirstMcCourseMod.java
@@ -13,10 +13,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(FirstMcCourseMod.MOD_ID)
 public class FirstMcCourseMod {
-    // Define mod id in a common place for everything to reference
+
     public static final String MOD_ID = "firstmccourse";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
