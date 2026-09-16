1005b30b2c2f4e4f4fbe158b8648adf0ebd5cb1c
jakobuscornel-ctrl <jakobuscornel@gmail.com>
2026-09-16 12:33:05 +0200

    Just made some changes!

--- src/main/java/net/chameleon/firstmccourse/FirstMcCourseMod.java
+++ src/main/java/net/chameleon/firstmccourse/FirstMcCourseMod.java
@@ -51,6 +51,7 @@ public class FirstMcCourseMod {
 
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
--- src/main/java/net/chameleon/firstmccourse/block/ModBlocks.java
+++ src/main/java/net/chameleon/firstmccourse/block/ModBlocks.java
@@ -26,6 +26,11 @@ public class ModBlocks {
                     .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
 
 
+    public static final RegistryObject<Block> RAW_ALEXANDRITE_BLOCK = registerBlock("raw_alexandrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
