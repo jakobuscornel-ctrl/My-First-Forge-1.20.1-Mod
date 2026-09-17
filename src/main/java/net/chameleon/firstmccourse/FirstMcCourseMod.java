f2503e183dba4845a92baf60d9879b688ee46174
jakobuscornel-ctrl <jakobuscornel@gmail.com>
2026-09-17 11:42:01 +0200

    Just made some changes!

--- src/main/java/net/chameleon/firstmccourse/FirstMcCourseMod.java
+++ src/main/java/net/chameleon/firstmccourse/FirstMcCourseMod.java
@@ -57,6 +57,7 @@ public class FirstMcCourseMod {
         if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
             event.accept(ModBlocks.ALEXANDRITE_ORE);
             event.accept(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.END_STONE_ALEXANDRITE_ORE);
         }
     }
 
--- src/main/java/net/chameleon/firstmccourse/block/ModBlocks.java
+++ src/main/java/net/chameleon/firstmccourse/block/ModBlocks.java
@@ -38,7 +38,7 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE)
                     .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
 
-    public static final RegistryObject<Block> END_STONE__ALEXANDRITE_ORE = registerBlock("end_stone_alexandrite_ore",
+    public static final RegistryObject<Block> END_STONE_ALEXANDRITE_ORE = registerBlock("end_stone_alexandrite_ore",
             () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE)
                     .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
 
